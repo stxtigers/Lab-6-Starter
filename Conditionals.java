d28af386a70ae46bac743fcc5b883b533247b503
Dan Able <abled@stxtigers.com>
2020-10-22 01:57:45 +0000

    Initial commit

---
+++ Conditionals.java
@@ -0,0 +1,35 @@
+java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    //Add code here to call your methods to test them
+    //When calling methods in the same class, you can just use the method name.  If no object or class is specified, this class is assumed (you can also use the this keyword...Mr. Able will do a few of these with you as examples)
+
+
+  }
+
+  //NOTE: MAKE ALL METHODS BELOW PUBLIC and STATIC.  This means they can be called from the main method without first creating an object.  Math.sqrt() and Math.pow() are examples of static methods.  Public lets my tester program call them
+
+  //leapYearA
+  public static boolean leapYearA(int year)
+  {
+    //Add your code here
+  }
+
+
+  //leapYearB
+
+
+
+  /*
+  inRange -- List test values for a, b and n here (see section 7.2 in the book for info on how to choose test cases:
+
+  */
+
+
+  
+  //birthday
+
+
+
+}
